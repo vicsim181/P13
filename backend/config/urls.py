25153001bb322dd0e3880bcddfb622ebda1f9aff
vicsim181 <63299151+vicsim181@users.noreply.github.com>
2021-07-11 11:40:28 +0200

    update 11/07

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('api-auth/', include('rest_framework.urls')),
     path('', include('applications.authentication.urls')),
 ]
