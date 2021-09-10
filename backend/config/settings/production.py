d329125b050d64a7dfd75d07d7b0a9b6c9269fee
vicsim181 <63299151+vicsim181@users.noreply.github.com>
2021-09-10 09:59:06 +0200

    sentry

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -19,3 +19,17 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+sentry_sdk.init(
+    dsn="https://1d4c9e58518f4fc18b84065cd6b4fe31@o622128.ingest.sentry.io/5953587",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
