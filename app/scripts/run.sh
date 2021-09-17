8db01967949a33050781286f2b8b851d7eec2295
vicsim181 <victor.simon@outlook.com>
2021-09-17 11:51:05 +0200

    Update run.sh

--- app/scripts/run.sh
+++ app/scripts/run.sh
@@ -10,6 +10,7 @@ whoami
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py db_types
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module config.wsgi
 
