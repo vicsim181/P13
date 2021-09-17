18ff873979bae00848362b3bcb6f84fc40954f1a
vicsim181 <victor.simon@outlook.com>
2021-09-17 15:36:26 +0200

    Update entrypoint.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 whoami
 
 python manage.py wait_for_db
-python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 python manage.py db_types
+python manage.py createsuperuser --noinput
