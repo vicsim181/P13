2583e047e13f7f6d4c1d1a3de8364a7de268c72d
vicsim181 <victor.simon@outlook.com>
2021-09-17 10:44:48 +0200

    Nginx added

--- app/scripts/run.sh
+++ app/scripts/run.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-# ls -la /vol/
-# ls -la /vol/web
+ls -la /vol/
+ls -la /vol/web
 
 whoami
 
 python manage.py wait_for_db
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module config.wsgi
