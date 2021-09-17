c9e568d9c3b917d42ed5eb40bfb1b7d6176ae884
vicsim181 <victor.simon@outlook.com>
2021-09-17 14:35:16 +0200

    update

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# set -e
-
-# ls -la /vol/
-# ls -la /vol/web
-
-# whoami
-
-# python manage.py wait_for_db
-# python manage.py collectstatic --noinput
-# python manage.py migrate --noinput
-# python manage.py db_types
-
-# gunicorn config.wsgi:application --bind 0.0.0.0:8000
-# # uwsgi --socket :9000 --workers 4 --master --enable-threads --module config.wsgi
-
-
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
