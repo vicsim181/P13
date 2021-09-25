e555591433d27a1b2eb949356078e6916ab2666b
vicsim181 <63299151+vicsim181@users.noreply.github.com>
2021-09-25 17:24:25 +0200

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -25,7 +25,7 @@ DATABASES = {
 
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    'https://participons.herokuapp.com/*'
+    'https://participons.herokuapp.com'
 ]
 
 # CORS_ALLOW_METHODS = [
