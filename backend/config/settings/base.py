94ad0f870d98e7cd2a9dfa100add97c677886564
vicsim181 <victor.simon@outlook.com>
2021-09-30 13:26:13 +0200

    Update base.py

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -50,10 +50,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -164,11 +164,6 @@ SIMPLE_JWT = {
 # DJANGO CORS HEADERS
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5000',
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://172.19.0.3:3000',
-    'http://127.0.0.1:8000',
     'http://*'
 ]
 
