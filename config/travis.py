31ae8b42dc1f49ed7f286c27897f5ff4acebe157
vicsim181 <victor.simon@outlook.com>
2021-09-30 16:11:15 +0200

    Update travis.py

--- config/travis.py
+++ config/travis.py
@@ -1,6 +1,8 @@
 from config.settings import *
 
 
+SECRET_KEY = 'supers€cretKey'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',  # we use the postgresql adaptator
