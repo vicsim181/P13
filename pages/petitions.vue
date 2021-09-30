6fb51aaa986cba9857c744bb9fe124998848672c
vicsim181 <victor.simon@outlook.com>
2021-09-30 18:11:23 +0200

    fix

--- pages/petitions.vue
+++ pages/petitions.vue
@@ -35,9 +35,9 @@ export default {
 
 <style scoped>
 .container {
-  width: 100%;
+  /* width: 100%; */
   min-width: 100%;
-  padding-top: 6rem;
+  padding-top: 13rem;
   padding-bottom: 6rem;
   color: rgb(0, 14, 116);
 }
@@ -54,7 +54,7 @@ export default {
   }
   .container {
     max-width: 50%;
-    padding-top: 5rem;
+    padding-top: 10rem;
     padding-bottom: 6rem;
     color: rgb(0, 14, 116);
   }
