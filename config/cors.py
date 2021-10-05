236b7042fcce4b23a36ff2ae3bed9c782fe7e27e
vicsim181 <victor.simon@outlook.com>
2021-10-05 18:50:14 +0200

    Update cors.py

--- config/cors.py
+++ config/cors.py
@@ -1,18 +1,18 @@
-from django import http
-import os
+# from django import http
+# import os
 
 
-class CorsMiddleware(object):
-    def __init__(self, get_response):
-        self.get_response = get_response
+# class CorsMiddleware(object):
+#     def __init__(self, get_response):
+#         self.get_response = get_response
 
-    def __call__(self, request):
-        response = self.get_response(request)
-        if (request.method == "OPTIONS" and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META):
-            response = http.HttpResponse()
-            response["Content-Length"] = "0"
-            response["Access-Control-Max-Age"] = 86400
-        response["Access-Control-Allow-Origin"] = os.environ.get('FRONT_URL')
-        response["Access-Control-Allow-Methods"] = "DELETE, GET, OPTIONS, PATCH, POST, PUT"
-        response["Access-Control-Allow-Headers"] = "accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with"
-        return response
+#     def __call__(self, request):
+#         response = self.get_response(request)
+#         if (request.method == "OPTIONS" and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META):
+#             response = http.HttpResponse()
+#             response["Content-Length"] = "0"
+#             response["Access-Control-Max-Age"] = 86400
+#         response["Access-Control-Allow-Origin"] = os.environ.get('FRONT_URL')
+#         response["Access-Control-Allow-Methods"] = "DELETE, GET, OPTIONS, PATCH, POST, PUT"
+#         response["Access-Control-Allow-Headers"] = "accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with"
+#         return response
