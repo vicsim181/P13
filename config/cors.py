e067ad16c365271e7464b3154f4ee32f95164531
vicsim181 <victor.simon@outlook.com>
2021-10-05 12:30:36 +0200

    Update cors.py

--- config/cors.py
+++ config/cors.py
@@ -1,4 +1,5 @@
 from django import http
+import os
 
 
 class CorsMiddleware(object):
@@ -7,11 +8,11 @@ class CorsMiddleware(object):
 
     def __call__(self, request):
         response = self.get_response(request)
-        if (request.method == "OPTIONS"  and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META):
+        if (request.method == "OPTIONS" and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META):
             response = http.HttpResponse()
             response["Content-Length"] = "0"
             response["Access-Control-Max-Age"] = 86400
-        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Origin"] = os.environ.get('FRONT_URL')
         response["Access-Control-Allow-Methods"] = "DELETE, GET, OPTIONS, PATCH, POST, PUT"
         response["Access-Control-Allow-Headers"] = "accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with"
         return response
